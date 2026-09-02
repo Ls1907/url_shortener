a7f2a8d9776874250417c16358b614a7ff19d04c
Kunwar Lav singh <dpsvaranasi619@gmail.com>
2026-09-03 01:01:05 +0530

    updated user schema and added validation for signup route using zod

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,12 +3,19 @@ import db from "../db/index.js";
 import { usersTable } from "../models/index.js";
 import { randomBytes, createHmac } from "node:crypto";
 import { eq } from "drizzle-orm";
+import {signupPostRequestBodySchema} from '../validation/request.validation.js'
 
 const router  = express.Router();
 
 
-router.get('/signup', async(req,res)=>{
-      const { name, email, password } = req.body;
+router.post('/signup', async(req,res)=>{
+      const validationResult = await signupPostRequestBodySchema.safeParseAsync(req.body);
+      if(validationResult.error){
+         return res.status(400).json({error: validationResult.error.format()});
+      }
+
+      const {firstName,lastName,email,password} = validationResult.data;
+      
       const [existingUser] = await db
          .select({ email: usersTable.email })
          .from(usersTable)
@@ -21,7 +28,8 @@ router.get('/signup', async(req,res)=>{
    const hashedPassword = createHmac("sha256", salt).update(password).digest("hex");
 
      const [newUser] = await db.insert(usersTable).values({
-        name,
+        firstName,
+        lastName,
         email,
         password: hashedPassword,
         salt
